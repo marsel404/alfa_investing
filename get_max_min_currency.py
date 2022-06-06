0c27c7a54cb370b8c09fc535e94984492ab4243b
marsel404 <104521338+marsel404@users.noreply.github.com>
2022-06-06 20:52:50 +0500

    Обновление подключения к БД

--- get_max_min_currency.py
+++ get_max_min_currency.py
@@ -5,12 +5,15 @@ def get_max_min_currency(currency_pair):
     try:
         connection = sqlite3.connect('db.sqlite3')
         cursor = connection.cursor()
+        currency_pair_get_id = cursor.execute(
+            "SELECT * FROM api_currencypairs WHERE currency_pair == ?", (currency_pair, )).fetchone()
+        id = currency_pair_get_id[0]
         min = cursor.execute(
-            "SELECT * FROM " + currency_pair + " ORDER BY min ").fetchone()
+            "SELECT * FROM api_currencypair WHERE currency_pair_id == ? ORDER BY min", (id, )).fetchone()
         max = cursor.execute(
-            "SELECT * FROM " + currency_pair + " ORDER BY -max ").fetchone()
+            "SELECT * FROM api_currencypair WHERE currency_pair_id == ? ORDER BY -max", (id, )).fetchone()
         connection.close()
-        print(str(currency_pair.upper()), min[1], max[2])
+        print(str(currency_pair.upper()), min[2], max[3])
     except:
         print('Неизвестный параметр. Пожалуйста, укажите корректную валютную пару.')
 
